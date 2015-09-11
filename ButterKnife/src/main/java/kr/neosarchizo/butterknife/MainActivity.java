fc33f6d4a20e681fe938125b57ec36ae90ea57bc
Junhyuk Lee <neosarchizo@gmail.com>
2015-09-11 09:58:41 +0900

    Complete ButterKnife

--- ButterKnife/src/main/java/kr/neosarchizo/butterknife/MainActivity.java
+++ ButterKnife/src/main/java/kr/neosarchizo/butterknife/MainActivity.java
@@ -1,37 +1,39 @@
 package kr.neosarchizo.butterknife;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.txtTitle)
+    TextView txtTitle;
+
+    @Bind({R.id.btnA, R.id.btnB, R.id.btnC})
+    List<Button> buttons;
+
+    @OnClick(R.id.btnA)
+    void button1() {
+        Toast.makeText(this, "Hello!!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        ButterKnife.bind(this);
+        txtTitle.setText("Hello~~~");
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText("Button " + (i + 1));
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
